ff42624eda6bf8e6ce2a11009b48ceb251dbe93f
Muhammad-Ahmed-Javed <123076043+AJSTYLE-lab@users.noreply.github.com>
2024-07-12 00:11:51 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,11 @@ import librosa
 import numpy as np
 import joblib
 import tempfile
+import requests
+def download_file(url, file_path):
+    with open(file_path, "wb") as f:
+        response = requests.get(url)
+        f.write(response.content)
 def extract_features(audio_file):
     y, sr = librosa.load(audio_file, sr=None)
     chroma_stft = np.mean(librosa.feature.chroma_stft(y=y, sr=sr))
@@ -14,10 +19,14 @@ def extract_features(audio_file):
     mfcc = np.mean(librosa.feature.mfcc(y=y, sr=sr), axis=1)
     features = np.array([chroma_stft, rmse, spectral_centroid, spectral_bandwidth, rolloff, zero_crossing_rate, *mfcc])
     return features
-model_path = "https://github.com/AJSTYLE-lab/AudioShield_Leveraging_Machine_Learning_to_Detect_Deepfake_Voices/blob/main/svm_model.pkl"
-scaler_path="https://github.com/AJSTYLE-lab/AudioShield_Leveraging_Machine_Learning_to_Detect_Deepfake_Voices/blob/main/scaler.pkl"
-svm_model=joblib.load(model_path)
-scaler=joblib.load(scaler_path)
+model_url = "https://github.com/AJSTYLE-lab/AudioShield_Leveraging_Machine_Learning_to_Detect_Deepfake_Voices/raw/main/svm_model.pkl"
+scaler_url = "https://github.com/AJSTYLE-lab/AudioShield_Leveraging_Machine_Learning_to_Detect_Deepfake_Voices/raw/main/scaler.pkl"
+model_path = "/tmp/svm_model.pkl"
+scaler_path = "/tmp/scaler.pkl"
+download_file(model_url, model_path)
+download_file(scaler_url, scaler_path)
+svm_model = joblib.load(model_path)
+scaler = joblib.load(scaler_path)
 st.title("AudioShield: Leveraging Machine Learning to Detect Deepfake Voices")
 st.write("Welcome to the DeepFake Audio Detection tool. This application leverages an SVM model to determine whether an audio file is real or fake.")
 uploaded_file = st.file_uploader("Choose an audio file", type=["flac"])
@@ -25,7 +34,7 @@ if uploaded_file:
     with tempfile.NamedTemporaryFile(delete=False, suffix='.flac') as temp_flac_file:
         temp_flac_file.write(uploaded_file.read())
         temp_flac_path = temp_flac_file.name
-    features = extract_features(temp_flac_path)    
+    features = extract_features(temp_flac_path)
     if features is not None:
         features_scaled = scaler.transform([features])
         features_scaled = features_scaled.reshape(1, -1)
